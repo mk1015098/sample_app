b252d5db25b02bbc020843e50200e95c5f8c5f65
MoekaKakimoto <k.moeka0984@gmail.com>
2022-11-10 00:23:55 +0000

    [Finish]3-12

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,12 +4,10 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(list_params)
-    if @list.save
-      redirect_to list_path(@list.id)
-    else
-      render :new
-    end  
+    list = List.new(list_params)
+    list.save
+    flash[:notice] = "投稿が成功しました"
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -29,11 +27,11 @@ class ListsController < ApplicationController
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
-    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
 
   private
